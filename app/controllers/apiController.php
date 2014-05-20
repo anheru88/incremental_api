23da0fb927865f37c87012044637eddd86a3eefd
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-05-20 14:23:51 -0500

    Uso de la clase IlluminateResponse, cambio de los codigos a las constantes de illumintaResponse, agregados nuevos metodos para http_create, and http_unprocess

--- app/controllers/apiController.php
+++ app/controllers/apiController.php
@@ -1,14 +1,31 @@
 <?php
 
+
+use Illuminate\Http\Response as IlluminateResponse;
+
 class ApiController extends BaseController {
 
-	protected $statusCode = 200;
+	/**
+	 * Status Code - Http status code - default 200
+	 * @var integer
+	 */
+	protected $statusCode = IlluminateResponse::HTTP_OK;
 
+	/**
+	 * Get StatusCode
+	 * @return integer StatusCode
+	 */
 	public function getStatusCode()
 	{
 		return $this->statusCode;
 	}
 
+
+	/**
+	 * Set StatusCode
+	 * @param integer $statusCode
+	 * @return  integer StatusCode
+	 */
 	public function setStatusCode($statusCode)
 	{
 		$this->statusCode = $statusCode;
@@ -16,21 +33,40 @@ class ApiController extends BaseController {
 		return $this;
 	}
 
+	/**
+	 * @param  string $message
+	 * @return mixed
+	 */
 	public function respondNotFound($message = 'Not Found!')
 	{
-		return $this->setStatusCode(404)->respondWithError($message);
+		return $this->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)->respondWithError($message);
 	}
 
+	/**
+	 * @param  string $message
+	 * @return mixed
+	 */
 	public function respondInternalError($message = 'Internal Error')
 	{
-		return $this->setStatusCode(500)->respondWithError($message);
+		return $this->setStatusCode(IlluminateResponse::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
 	}
 
+	/**
+	 * [respond description]
+	 * @param  [type] $data
+	 * @param  [type] $headers
+	 * @return [type]
+	 */
 	public function respond($data, $headers = [])
 	{
 		return Response::json($data, $this->getStatusCode(), $headers);
 	}
 
+	/**
+	 * [respondWithError description]
+	 * @param  [type] $message
+	 * @return [type]
+	 */
 	public function respondWithError($message)
 	{
 		return $this->respond([
@@ -38,6 +74,30 @@ class ApiController extends BaseController {
 				'message' => $message,
 				'status_code' => $this->getStatusCode()
 			]
+		]);
+	}
+
+	/**
+	 * [respondCreated description]
+	 * @param  [type] $message
+	 * @return [type]
+	 */
+	public function respondCreated($message)
+	{
+		return $this->setStatusCode(IlluminateResponse::HTTP_CREATED)->respond([
+			'message' => $message
+		]);
+	}
+	
+	/**
+	 * [respondUnprocess description]
+	 * @param  [type] $message
+	 * @return [type]
+	 */
+	public function respondUnprocess($message)
+	{
+		return $this->setStatusCode(IlluminateResponse::HTTP_UNPROCESSABLE_ENTITY)->respond([
+			'message' => $message
 		]);
 	}
 }
