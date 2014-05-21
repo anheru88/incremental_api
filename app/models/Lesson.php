cc34b2f774e3a098345e4d3818ecc91c591f0a13
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2014-05-21 13:24:13 -0500

    Creado el método tags en el modelo de Lecciones para mostrar los tags asociados.

--- app/models/Lesson.php
+++ app/models/Lesson.php
@@ -2,4 +2,13 @@
 
 class Lesson extends \Eloquent {
 	protected $fillable = ['title', 'body'];
+
+	/**
+	 * Regresa los tags asociados con la lección.
+	 * @return Array de Tags
+	 */
+	public function tags()
+	{
+		return $this->belongsToMany('Tag');
+	}
 }	
