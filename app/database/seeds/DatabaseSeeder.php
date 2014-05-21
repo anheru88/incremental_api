0656c28eef302780ca190d1ca39c609d5a7bf730
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-05-21 10:33:27 -0500

    Database Seeder Update.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,13 +9,19 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS=0');
 		Lesson::truncate();
 		User::truncate();
+		Tag::truncate();
+		DB::table('lesson_tag')->truncate();
+		DB::statement('SET FOREIGN_KEY_CHECKS=1');
 		
 		Eloquent::unguard();
 
 		$this->call('LessonsTableSeeder');
 		$this->call('UsersTableSeeder');
+		$this->call('TagsTableSeeder');
+		$this->call('LessonTagTableSeeder');
 	}
 
 }
