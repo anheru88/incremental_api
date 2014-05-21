70f123194c58ebc7bcbf9ee7e7ff6d0ad4f152f4
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2014-05-21 13:06:35 -0500

    Agregada la ruta de Tags  -> TagsController, only index and show.

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,5 @@
 
 Route::group(['prefix' => 'api/v1'], function(){
 	Route::resource('lessons', 'LessonsController');
+	Route::resource('tags', 'TagsController', ['only' => ['index', 'show']]);
 });
