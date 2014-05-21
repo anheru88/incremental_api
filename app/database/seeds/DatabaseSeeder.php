4b0bf111a3d2f435f92e4b9e8a46d9eb95d07e04
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2014-05-21 11:18:47 -0500

    Creado el metido cleanDatabase, y el arreglo de los nombres de tablas.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 class DatabaseSeeder extends Seeder {
 
+	private $tables = [
+		'users',
+		'lessons',
+		'tags',
+		'lesson_tag'
+	];
+
 	/**
 	 * Run the database seeds.
 	 *
@@ -9,12 +16,7 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		DB::statement('SET FOREIGN_KEY_CHECKS=0');
-		Lesson::truncate();
-		User::truncate();
-		Tag::truncate();
-		DB::table('lesson_tag')->truncate();
-		DB::statement('SET FOREIGN_KEY_CHECKS=1');
+		$this->cleanDatabase();
 		
 		Eloquent::unguard();
 
@@ -22,6 +24,17 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UsersTableSeeder');
 		$this->call('TagsTableSeeder');
 		$this->call('LessonTagTableSeeder');
+	}
+
+	private function cleanDatabase()
+	{
+		DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+		foreach ($this->tables as $tableName) {
+			DB::table($tableName)->truncate();
+		}
+		
+		DB::statement('SET FOREIGN_KEY_CHECKS=1');
 	}
 
 }
